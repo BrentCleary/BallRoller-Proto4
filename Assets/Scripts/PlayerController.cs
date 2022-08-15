cade19dc6d540ace81bc8598f7918837efe3ff6d
Brent Cleary <69173728+BrentCleary@users.noreply.github.com>
2022-08-15 02:55:42 -0700

    2nd Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,20 @@ public class PlayerController : MonoBehaviour
     public GameObject powerupIndicator;
     public float powerupRotationSpeed = 2.0f;
     public float powerupTimer = 10;
+    public int missleCount;
+    private Vector3 missleFireOffset = new Vector3(0f, 1.5f, 0f);
+    public int breakFriction = 2;
+    public bool isOnGround = false;
+
+    public GameObject misslePrefab;
+    public Rigidbody missleRb;
+    public float fallingVelocity; 
+    private Vector3 lastposition;
+
+    // Homing variables
+    public GameObject[] enemiesInScene;
+    public float missleSpeed = 10f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +33,8 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();    
         focalPoint = GameObject.Find("FocalPoint");    
         powerupStrength = speed;
+        lastposition = transform.position;  // sets lastPosition for DetectFalling()
+        missleRb = misslePrefab.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -30,6 +46,11 @@ public class PlayerController : MonoBehaviour
 
         powerupIndicator.transform.position = transform.position;
         powerupIndicator.transform.Rotate(0, powerupRotationSpeed, 0);
+
+        FireMissle();
+        DetectAirborne();
+        SlowToStop();
+
     }
 
     void OnTriggerEnter(Collider other)
@@ -59,6 +80,89 @@ public class PlayerController : MonoBehaviour
             enemyRb.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
             Debug.Log("Collided with : " + collision.gameObject.name + " and powerup set to " + hasPowerup);
         }
+
+
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        // playerRB moves up and down during horizontal movement
+        // setting the velocity trigger to 4 ensure it occurs with large movements.
+
     }
+
+    // The script below should calculate the rotation between the player and the enemy
+    // and store the result in missleAngle
+    // and Instantiate the missle pointing in it's direction.
+    void FireMissle()
+    {        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            missleCount++;
+            Quaternion missleAngle;
+
+            enemiesInScene = GameObject.FindGameObjectsWithTag("Enemy");
+            for(int i = 0; i < 1; i++)
+            {
+                Vector3 enemyDirection = (enemiesInScene[i].transform.position - transform.position).normalized;
+                Vector3 rotateAmount = Vector3.Cross(enemyDirection, transform.up);
+                Vector3 missleDirection = (enemyDirection - playerRb.position);
+
+                missleAngle = new Quaternion(rotateAmount.x, rotateAmount.y, rotateAmount.z, 0);
+
+                Instantiate(misslePrefab, transform.position + missleFireOffset, missleAngle);
+            }
+        }
+    }
+
+    void SlowToStop()
+    {
+        if(Input.GetKey(KeyCode.E) && isOnGround)
+        {
+            playerRb.velocity = new Vector3(playerRb.velocity.x / breakFriction, playerRb.velocity.y / breakFriction, playerRb.velocity.z / breakFriction);
+        }
+    }
+
+    void DetectAirborne()
+    {
+        fallingVelocity = (transform.position.y - lastposition.y);
+        lastposition = transform.position;
+
+        if(fallingVelocity > .1f || fallingVelocity < -.1f)
+        {
+            isOnGround = false;
+        }
+
+        // // The below script caluclates the speed between current and last position.
+        // fallingVelocity = (transform.position.y - lastposition.y);
+        // lastposition = transform.position;
+
+        // if(fallingVelocity != 0)
+        // {
+        //     isOnGround = false;
+        // }
+    }
+
+
+    // // Identifies enemies in scene
+    // // Compares their distance to the player
+    // // Returns the index of the shortest distance enemy
+    // // Sorting to lowest could use improvement
+    // public int Proximity(int closestEnemy, GameObject[] enemiesInScene)
+    // {
+    //     closestEnemy = 0;
+    //     for(int i = 0; i < enemiesInScene.Length; i++)
+    //     {
+    //         Vector3 enemyDistance = (enemiesInScene[i].transform.position - transform.position);
+
+    //         if(enemyDistance[i] < enemyDistance[i+1])
+    //         {
+    //             closestEnemy = i;
+    //         }
+    //     }
+
+    //     return closestEnemy;
+    // }
+
 
 }
